1f3a597c7a99dc5ef184bef61dbce7b6b1932135
Fernando Furtado <fppfurtado@outlook.com>
2024-12-19 19:46:30 -0300

    cria metodo setup_api_client

--- trader.py
+++ trader.py
@@ -1,5 +1,18 @@
+import os
+from binance.client import Client
+from dotenv import load_dotenv
+
+def __setup_api_client():
+    load_dotenv()
+    API_KEY = os.environ['BINANCE_API_KEY']
+    API_SECRET = os.environ['BINANCE_API_SECRET']
+    return Client(API_KEY, API_SECRET)
+
+
 def __main():
-    print('Hello World')
+    
+    api_client = __setup_api_client()
+    print(api_client.get_server_time())
 
 if __name__ == '__main__':
     __main()
