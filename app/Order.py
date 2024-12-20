973cb7f185ffbbe29d85abc7a47495700b5b3257
Fernando Furtado <fppfurtado@outlook.com>
2024-12-20 06:34:19 -0300

    cria classes Trade, Order e TradeStrategy

---
+++ app/Order.py
@@ -0,0 +1,27 @@
+class Order:
+
+    BUY: str = 'BUY'
+    SELL: str = 'SELL'
+    
+    def __init__(self, side: str, symbol: str, quantity: decimal, price: str):
+        self._side = side
+        self._symbol = symbol
+        self._quantity = quantity
+        self._price
+
+    @property
+    def side() -> str:
+        return self._side
+
+    @property
+    def symbol() -> str:
+        return self._symbol
+
+    @property
+    def quantity():
+        return self._quantity
+
+    @property
+    def price():
+        return self._price
+    
