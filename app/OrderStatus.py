7b8d6fec3d313dd613d8835dad40a955ea3f339a
Fernando Furtado <fppfurtado@outlook.com>
2024-12-22 14:18:27 -0300

    deleta arquivos das enums após remanejar classes para módulo Order.py

--- app/OrderStatus.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum
-
-class OrderStatus(Enum):
-    NEW = 'NEW'
-    FILLED = 'FILLED'
-    CANCELED = 'CANCELED'
--- app/OrderTime.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-class OrderTime(Enum):
-    GOOD_TIL_CANCELED = 'GTC'
-    IMMEDIATE_OR_CANCEL = 'IOC'
-    FILL_OR_KILL = 'FOK'
-    GOOD_TIL_CROSS = 'GTX'
-    
