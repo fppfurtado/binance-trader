bcec5005b84d81a6f974337c3d68c81bdbe3092f
Fernando Furtado <fppfurtado@outlook.com>
2024-12-22 09:43:09 -0300

    ajusta nome de variaveis e imports

--- app/Trade.py
+++ app/Trade.py
@@ -1,21 +1,20 @@
-import decimal
 import time
-import TradeRole
-import TradeSide
+import decimal
+import TradeRole, TradeSide
 from dataclasses import dataclass
 
 @dataclass
 class Trade:
     time: time = time()
+    side: TradeSide
+    role: TradeRole
+    base_asset: str
+    quote_asset: str
     price: decimal
     quantity: decimal
-    role: TradeRole
-    baseAsset: str
-    quoteAsset: str
-    side: TradeSide
-    feeAsset: str
-    totalQuota: decimal
+    fee_asset: str
+    total_quota: decimal
 
     @property
     def symbol(self):
-        return self.baseAsset + self.quoteAsset
+        return self.base_asset + self.quote_asset
