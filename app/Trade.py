17d24e4821f84139faa4dc6bc6d6f37d9522a6d1
Fernando Furtado <fppfurtado@outlook.com>
2024-12-22 09:08:17 -0300

    implementa a classe Trade segundo o modelo da binance

--- app/Trade.py
+++ app/Trade.py
@@ -1,25 +1,18 @@
 import decimal
-from Position import Position
+import time
+import TradeRole
+import TradeSide
+from dataclasses import dataclass
 
+@dataclass
 class Trade:
-
-    def __init__(self, open_position: Position, close_position: Position):
-        self.client = client
-        self._open_position = open_position
-        self._close_position = close_position
-
-    @property
-    def open_position(self):
-        return self._open_position
-
-    @open_position.setter
-    def open_position(self, position: Position):
-        self._open_position = position
-
-    @property
-    def close_position(self):
-        return self._close_position
-
-    @close_position.setter
-    def close_position(self, position: Position):
-        self._close_position = position
+    time: time = time()
+    symbol: str
+    price: decimal
+    quantity: decimal
+    role: TradeRole
+    baseAsset: str
+    quoteAsset: str
+    side: TradeSide
+    feeAsset: str
+    totalQuota: decimal
