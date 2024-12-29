01220b6109e40429b55254ad1fdad6dd4395c2d4
Fernando Furtado <fppfurtado@outlook.com>
2024-12-29 11:24:24 -0300

    refatora classe PriceMonitor para usar somente websocket e implementa get_current_prices

--- app/monitoring.py
+++ app/monitoring.py
@@ -1,69 +1,58 @@
-from binance.client import Client
 from collections import deque
 from threading import Lock
-import pandas as pd
-from typing import Deque, List
-import time
-from datetime import datetime, timedelta
-from api_client import BaseClient
+from typing import Deque, List, Tuple
 
 class PriceMonitor:
-    def __init__(self, client: BaseClient, socket_manager, max_length: int = 250):
-        self.client = client
+    def __init__(self, symbol: str, socket_manager, max_length: int = 250):
+        self.symbol = symbol
         self.socket_manager = socket_manager
         self.max_length = max_length
-        self.candles: Deque[List] = deque(maxlen=max_length)
-        self.closing_prices: Deque[float] = deque(maxlen=max_length)
-        self.timestamps: Deque[int] = deque(maxlen=max_length)
+        self.candles: Deque[Tuple] = deque(maxlen=max_length)
+        self.current_prices: Deque[Tuple] = deque(maxlen=max_length)
         self.lock = Lock()
         
-    def _initialize_history(self) -> None:
-        start_time = datetime.now() - timedelta(minutes=self.max_length)
-        klines = self.client.get_historical_data(
-            "BTCUSDT", 
-            Client.KLINE_INTERVAL_1MINUTE,
-            self.max_length
-        )
-        
-        with self.lock:
-            for kline in klines[-self.max_length:]:
-                self.timestamps.append(int(kline[0]))
-                self.closing_prices.append(float(kline[4]))
-                self.candles.append(kline[1:5])
-
-    def process_message(self, msg: dict) -> None:
+    def process_kline_message(self, msg: dict) -> None:
         if msg['e'] == 'kline' and msg['k']['i'] == '1m':
-            timestamp = msg['k']['t']
-            price = float(msg['k']['c'])
-            candle = [
-                msg['k']['o'],
-                msg['k']['c'],
-                msg['k']['h'],
-                msg['k']['l']
-            ]
+            candle = (
+                msg['k']['t'], # timestamp
+                msg['k']['o'], # open price
+                msg['k']['c'], # close price
+                msg['k']['h'], # high price
+                msg['k']['l']  # low price
+            )
             
             with self.lock:
-                if not self.timestamps or timestamp > self.timestamps[-1]:
-                    self.timestamps.append(timestamp)
-                    self.closing_prices.append(price)
-                    self.candles.append(candle)
-                else:
-                    self.closing_prices[-1] = price
-                    self.candles[-1] = candle
+                self.candles.append(candle)
+
+    def process_trade_message(self, msg: dict) -> None:
+        if msg['e'] == 'trade':  # Evento de trade (negociação)
+            preco = (
+                msg['E'],   # timestamp
+                msg['p']    # Preço da negociação
+            )
+            
+            with self.lock:
+                self.current_prices.append(preco)
+
+    def get_current_prices(self):
+        with self.lock:
+            return [price[1] for price in self.current_prices]
 
     def get_closing_prices(self) -> List[float]:
         with self.lock:
-            return [candle[1] for candle in self.candles]
+            return [candle[2] for candle in self.candles]
             
     def get_candles(self) -> List[List]:
         with self.lock:
             return list(self.candles)
 
     def start(self) -> None:
-        self._initialize_history()
         self.socket_manager.start()
+        self.socket_manager.start_trade_socket(
+            symbol=self.symbol,
+            callback=self.process_trade_message
+        )
         self.socket_manager.start_kline_socket(
             symbol="BTCUSDT",
-            callback=self.process_message,
-            interval=Client.KLINE_INTERVAL_1MINUTE
+            callback=self.process_kline_message
         )
