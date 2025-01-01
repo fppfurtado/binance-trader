05769b9a2ad69cd3d94f04f834ff64b322e067cf
Fernando Furtado <fppfurtado@outlook.com>
2025-01-01 13:49:31 -0300

    implementa estrutura basica da classe Strategy do pacote backtrader para a classe DefaultStrategy

--- app/strategies.py
+++ app/strategies.py
@@ -1,4 +1,13 @@
 import backtrader as bt
 
 class DefaultStrategy(bt.Strategy):
-    pass
+    def log(self, txt, dt=None):
+        '''Logging function for the strategy'''
+        dt = dt or self.datas[0].datetime.date(0)
+        print('%s, %s' % (dt.isoformat(), txt))
+
+    def __init__(self):
+        self.dataclose = self.datas[0].close
+
+    def next(self):
+        self.log('Close, %.2f' % self.dataclose[0])
