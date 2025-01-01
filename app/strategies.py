b757c308bf7c9622729b011705573afad4aa1348
Fernando Furtado <fppfurtado@outlook.com>
2025-01-01 15:36:10 -0300

    altera print de data da mensagem de log para exibir horas, minutos e segundos

--- app/strategies.py
+++ app/strategies.py
@@ -3,8 +3,8 @@ import backtrader as bt
 class DefaultStrategy(bt.Strategy):
     def log(self, txt, dt=None):
         '''Logging function for the strategy'''
-        dt = dt or self.datas[0].datetime.date(0)
-        print('%s, %s' % (dt.isoformat(), txt))
+        dt = dt or self.datas[0].datetime.datetime()
+        print('%s, %s' % (dt, txt))
 
     def __init__(self):
         self.dataclose = self.datas[0].close
