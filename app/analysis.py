6e3c23bea857887f8648ab682e8c8d01a1535082
Fernando Furtado <fppfurtado@outlook.com>
2025-01-01 09:00:55 -0300

    trata window = None em métodos de analysis.py

--- app/analysis.py
+++ app/analysis.py
@@ -1,10 +1,14 @@
 from typing import Protocol, List, Tuple
 import pandas as pd
 
-def get_exponential_moving_average(historical_data: [], length: int) -> float:
+def get_exponential_moving_average(historical_data: [], length: int = None) -> float:
+    if length is None:
+        length = len(historical_data)
+    
     return -1 if len(historical_data) == 0 else pd.Series(historical_data).ewm(span=length, adjust=False).mean().iloc[-1]
 
-def get_support_price(historical_data: [], window: int):
-    print(f'TAMANHO: {len(historical_data[-window:])}')
-    print(historical_data[-window:])
-    return -1 if len(historical_data) <= window else min(candle[3] for candle in historical_data[-window:])
+def get_support_price(historical_data: [], window: int = None):
+    if window is None:
+        window = len(historical_data)
+
+    return -1 if len(historical_data) < window else min(candle[3] for candle in historical_data[-window:])
