e32c26a219fc82425e28dcb3d32a2e1a679df867
Fernando Furtado <fppfurtado@outlook.com>
2025-01-01 15:44:07 -0300

    altera main para começar a usar backtrader

--- app/trader.py
+++ app/trader.py
@@ -2,6 +2,8 @@ import os, time
 from dotenv import load_dotenv
 import analysis
 from broker import BaseClient, BinanceClient
+import backtrader as bt
+from strategies import DefaultStrategy
 
 asset_symbol: str = None
 broker: BaseClient = None
@@ -26,11 +28,27 @@ def __init():
 def __main():
 
     __init()
+
+    cerebro = bt.Cerebro()
+
+    cerebro.addstrategy(DefaultStrategy)
+    data_feed = bt.feeds.PandasData(dataname=broker.get_historical_data(asset_symbol))
+    cerebro.adddata(data_feed)
+    cerebro.broker.set_cash(10000.0)
+
+    # Print out the starting conditions
+    print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
+
+    # Run over everything
+    cerebro.run()
+
+    # Print out the final result
+    print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
     
-    while broker is not None:
-        support = analysis.get_support_price(broker.get_historical_data(asset_symbol))
-        print(support)
-        time.sleep(1)
+    # while broker is not None:
+    #     support = analysis.get_support_price(broker.get_historical_data(asset_symbol))
+    #     print(support)
+    #     time.sleep(1)
 
 if __name__ == '__main__':
     __main()
