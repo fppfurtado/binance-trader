0f8001fcc00e810cf7dd0a9b9146e7a291814f83
Fernando Furtado <fppfurtado@outlook.com>
2025-01-01 12:22:26 -0300

    refatora market_data.py e renomeia para broker.py

--- app/broker.py
+++ app/broker.py
@@ -3,9 +3,11 @@ from typing import Protocol, Deque, List, Tuple
 from binance.client import Client
 from collections import deque
 from threading import Lock
+from binance.client import Client
+from binance.ws.streams import ThreadedWebsocketManager
 
 class BaseClient(Protocol):
-    def get_system_status():
+    def get_system_status() -> dict:
         pass
 
     def order_market_buy(self, symbol: str, quote_order_qty: decimal):
@@ -17,46 +19,42 @@ class BaseClient(Protocol):
     def get_current_price(self, symbol: str) -> decimal:
         pass
 
-    def get_historical_data(self, symbol: str, interval: str, period:int):
+    def get_historical_data(self, symbol: str, interval: str, period:int) -> []:
         pass
 
-
-class BinanceClient(BaseClient):
-    def __init__(self, api_key, api_secret):
-        self.connector = Client(api_key, api_secret)
-    
-    def get_system_status(self):
-        return self.connector.get_system_status()
-
-    def order_market_buy(self, symbol: str, quote_order_qty: decimal):
-        return self.connector.order_market_buy(symbol=symbol, quoteOrderQty=quote_order_qty)
-
-    def order_limit_buy(self, symbol: str, price: decimal, quantity: decimal):
-        return self.connector.order_limit_buy(symbol=symbol, price=str(price), quantity=quantity)
-
-    def get_current_price(self, symbol: str) -> decimal:
-        return self.connector.get_symbol_ticker(symbol=symbol)
-
-    def get_historical_data(self, symbol: str, interval: str, limit:int = 50):
-        return self.connector.get_historical_klines(symbol=symbol, interval=interval, limit=limit)
-
-
-class BaseWebsocket(Protocol):
-    def get_last_trade(self):
+    def get_last_trade(self) -> dict:
         pass
 
     def get_candles(self) -> List[List]:
         pass
 
-class BinanceWebsocket(BaseWebsocket):
-    def __init__(self, symbol: str, socket_manager, max_length: int = 250):
+class BinanceClient(BaseClient):
+    def __init__(self, api_key, api_secret, symbol: str, candles_window: int = 250):
+        self.client = Client(api_key, api_secret)
+        self.socket_manager = ThreadedWebsocketManager(api_key=api_key, api_secret=api_secret)
         self.symbol = symbol
-        self.socket_manager = socket_manager
-        self.max_length = max_length
-        self.candles: Deque[Tuple] = deque(maxlen=max_length)
+        self.candles_window = candles_window
+        self.candles: Deque[Tuple] = deque(maxlen=candles_window)
         self.lock = Lock()
-        
-    def process_kline_message(self, msg: dict) -> None:
+
+        self.start()
+    
+    def get_system_status(self):
+        return self.client.get_system_status()
+
+    def order_market_buy(self, symbol: str, quote_order_qty: decimal):
+        return self.client.order_market_buy(symbol=symbol, quoteOrderQty=quote_order_qty)
+
+    def order_limit_buy(self, symbol: str, price: decimal, quantity: decimal):
+        return self.client.order_limit_buy(symbol=symbol, price=str(price), quantity=quantity)
+
+    def get_current_price(self, symbol: str) -> decimal:
+        return self.client.get_symbol_ticker(symbol=symbol)
+
+    def get_historical_data(self, symbol: str, interval: str = Client.KLINE_INTERVAL_1MINUTE, limit:int = 50):
+        return self.client.get_historical_klines(symbol=self.symbol, interval=interval, limit=limit)
+
+    def _process_kline_message(self, msg: dict) -> None:
         if msg['e'] == 'kline' and msg['k']['i'] == '1m':
             candle = (
                 msg['k']['t'], # timestamp
@@ -69,7 +67,7 @@ class BinanceWebsocket(BaseWebsocket):
             with self.lock:
                 self.candles.append(candle)
 
-    def process_trade_message(self, msg: dict) -> None:
+    def _process_trade_message(self, msg: dict) -> None:
         if msg['e'] == 'trade':  # Evento de trade (negociação)
             preco = (
                 msg['E'],   # timestamp
@@ -95,9 +93,9 @@ class BinanceWebsocket(BaseWebsocket):
         self.socket_manager.start()
         self.socket_manager.start_trade_socket(
             symbol=self.symbol,
-            callback=self.process_trade_message
+            callback=self._process_trade_message
         )
         self.socket_manager.start_kline_socket(
             symbol=self.symbol,
-            callback=self.process_kline_message
+            callback=self._process_kline_message
         )
