b331fbaa6c78cf6cba180e9a37d47c0e138279e4
Fernando Furtado <fppfurtado@outlook.com>
2025-01-26 00:59:14 -0300

    cria analysers ProfitReturns, TradeCounter e LastDate

--- app/analysis.py
+++ app/analysis.py
@@ -43,10 +43,66 @@ class ProfitReturns(bt.Analyzer):
 
     def stop(self):
         # O valor final do caixa no final do backtest
-        self.total_profit = self.strategy.total_profit
+        self.total_profit = round((self.strategy.total_profit / self.strategy.p.stake) * 100, 1)
 
     def get_analysis(self):
         # Calculando o retorno com base na evolução do caixa
         return {
             'total_profit': self.total_profit,
+        }
+
+class TradeCounter(bt.Analyzer):
+    def __init__(self):
+        super(TradeCounter, self).__init__()
+        # self.initial_profit = None
+        # self.final_profit = None
+        # self.profit_returns = []
+        # self.total_profit = 0
+
+    def start(self):
+        # Armazenando o valor inicial do caixa no início do backtest
+        # self.initial_profit = self.strategy.broker.startingcash
+        pass
+
+    def next(self):
+        # Armazenando o valor do caixa a cada passo do backtest
+        # self.profit_returns.append(self.strategy.broker.get_cash())
+        pass
+
+    def stop(self):
+        # O valor final do caixa no final do backtest
+        self.total_trades = self.strategy.executed_sell_orders_counter
+
+    def get_analysis(self):
+        # Calculando o retorno com base na evolução do caixa
+        return {
+            'total_trades': self.total_trades,
+        }
+
+class LastDate(bt.Analyzer):
+    def __init__(self):
+        super(LastDate, self).__init__()
+        # self.initial_profit = None
+        # self.final_profit = None
+        # self.profit_returns = []
+        # self.total_profit = 0
+
+    def start(self):
+        # Armazenando o valor inicial do caixa no início do backtest
+        # self.initial_profit = self.strategy.broker.startingcash
+        pass
+
+    def next(self):
+        # Armazenando o valor do caixa a cada passo do backtest
+        # self.profit_returns.append(self.strategy.broker.get_cash())
+        pass
+
+    def stop(self):
+        # O valor final do caixa no final do backtest
+        self.date = self.strategy.last_trade_date
+
+    def get_analysis(self):
+        # Calculando o retorno com base na evolução do caixa
+        return {
+            'last_date': self.date,
         }
